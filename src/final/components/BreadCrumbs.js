20a25497bc13ad023684eae3949aab92fff60199
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-02-23 08:35:40 +0330

    remove console.log

--- src/final/components/BreadCrumbs.js
+++ src/final/components/BreadCrumbs.js
@@ -3,7 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 export default function BreadCrumbs() {
   const location = useLocation();
   const crumbs = location.pathname.split("/").filter((c) => c !== "");
-  console.log(crumbs);
   return (
     <div>
       {crumbs.map((c) => (
